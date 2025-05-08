ed78b592eae5750484775464d1af745c4c77b8a1
Riley Berman <riley.berman@gmail.com>
2025-05-07 17:21:39 -0700

    Riley Berman -- Put others file in approprite files, deleted repeats, organized everything, removed Python API script with my access_key (DON'T USE).

---
+++ CA_Assembly/CA_25-26_Regular_Session.R
@@ -0,0 +1,26 @@
+#Contributor: Min Kim
+#Description: Created a table of the bills from CA 2025-2026 Regular Session containing
+#the bill numbers, bill titles, sponsors' names, sponsors' party affiliation, sponsors' roles (assembly or senate),
+#and links to bill info
+#Methodology: Downloaded the dataset from Legiscan and merged the tables using common categories
+
+
+rm(list=ls())
+
+install.packages("tidyverse")
+library("tidyverse")
+
+#Data from CA 2025-2026 Regular Session
+bill_info <- read_csv("Downloads/CA 2/2025-2026_Regular_Session/csv/bills.csv")
+spons_info <- read_csv("Downloads/CA 2/2025-2026_Regular_Session/csv/sponsors.csv")
+ppl_info <- read_csv("Downloads/CA 2/2025-2026_Regular_Session/csv/people.csv")
+
+merged_data <- left_join(spons_info, ppl_info, bill_info, by = "people_id") 
+#merged data of the three tables above. Lists the bill number, bill sponsors, sponsors' affiliation party, etc. 
+merged_df <- left_join(merged_data, bill_info, by = "bill_id")
+
+#final merged info with bill number, status, title, sponsors' name, party affiliations
+Merged_info <- merged_df %>%  select(bill_number, status_desc, title, name, party, role, url, state_link)
+
+
+#We would have to analyze the sponsors' party affiliations for each bill to identify if it's bipartisan?
