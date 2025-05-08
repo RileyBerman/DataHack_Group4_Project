e6d01e95f8ce03f285d07f1ece86d2478f5268d1
Riley Berman <riley.berman@gmail.com>
2025-05-07 17:11:48 -0700

    Riley Berman -- Rearranged project subfolders, and added LegiScan data and R file.

---
+++ LegiScan/RileyBerman_LegiScan_API.py
@@ -0,0 +1,83 @@
+#Contributor: Riley Berman 
+#Description: Retrieves datasets containing all bills, votes, and people information for every specified session available in California. Outputs a folder containing all such files in .csv format. 
+#Methodology: Using operations (getDataList and getDataset) documented in the LegiScan API https://legiscan.com/legiscan, interacts with the API to retrieve relevant datasets. 
+
+import requests
+import zipfile
+import os
+import io
+import base64
+
+#Note: if you want to run this code on your own computer you need to generate your own LegiScan API key 
+
+#Personal API Key (put your own if you are running this code)
+API_Key = "ff23a4323f61c8159fd9cb3a3e578a6b"
+#Extracting California legislative data
+State = "CA"
+
+#Using getDatasetList operation to retreive session_id's and access_key's for getDataset operation
+url = f"https://api.legiscan.com/?key={API_Key}&op=getDatasetList&state={State}"
+response = requests.get(url)
+data = response.json()
+
+#Creating list of session_id, access_key pairs
+session_access_pair = []
+for element in data["datasetlist"]: 
+  session_id = element["session_id"]
+  access_key = element["access_key"]
+  session_access_pair.append([session_id, access_key])
+
+for pair in session_access_pair:  
+  #Using getDataset operation to retrieve base64 encoded ZIP file containing all bills, votes and people data for the specified session (in .csv format)
+  #pair[0] is the session_id, pair[1] is the access_key
+  url = f"https://api.legiscan.com/?key={API_Key}&op=getDataset&id={pair[0]}&access_key={pair[1]}&format=csv"
+  response = requests.get(url)
+  data = response.json()
+
+  base64_encoded_zip = data['dataset']['zip']
+
+  #Decoding base64 zip string 
+  zip_bytes = base64.b64decode(base64_encoded_zip)
+
+  #Create an in-memory zip file object
+  zip_file = io.BytesIO(zip_bytes)
+
+  #Unzipping files and saving them to a specified directory 
+  with zipfile.ZipFile(zip_file, 'r') as zf:
+    #Specify directory (Data Hack folder on Desktop for me)
+    output_directory = "/Users/rileyberman/Desktop/Data Hack"
+    
+    #Creates directory if nonexistent
+    os.makedirs(output_directory, exist_ok=True) 
+    
+    #Extracts all files to specified directory
+    zf.extractall(output_directory)
+
+    print(f"Successfully unzipped files for session_id {pair[0]} with access_key {pair[1]} to: {output_directory}")
+
+'''The code above should be sufficient, however, I have included some code implementing other API operations below for future reference. 
+
+#Using getSessionList operation to get session_id's
+url = f"https://api.legiscan.com/?key={API_Key}&op=getSessionList&state={State}"
+response = requests.get(url)
+data = response.json()
+
+#Getting all available session_id's 
+session_ids = []
+for session in data["sessions"]: 
+  session_ids.append(session["session_id"])
+
+#Using getMasterList operation to get bill_id's for a specific session
+url = f"https://api.legiscan.com/?key={API_Key}&op=getMasterList&id={session_ids[1]}"
+response = requests.get(url)
+data = response.json()
+
+#Getting all available bill_id's
+bill_ids = []
+for element in data["masterlist"]: 
+
+  #with session_id, there is a first element 'session'
+  if element == "session":
+    continue 
+
+  bill_ids.append(data["masterlist"][element]["bill_id"])'''
---
+++ Twitter/RileyBerman_API_Test1.py
@@ -0,0 +1,43 @@
+import requests
+import pandas as pd
+
+baseurl = 'https://rickandmortyapi.com/api/'
+
+endpoint = 'character'
+
+def main_request(baseurl, endpoint, x): 
+  r = requests.get(baseurl + endpoint + f'?page={x}')
+  return r.json()
+
+def get_pages(response): 
+  return response['info']['pages'] 
+
+def parse_json(response): 
+  charlist = []
+  for item in response['results']: 
+    #print(item['name'], len(item['episode']))
+    char = {
+      'id': item['id'],
+      'name': item['name'],
+      'no_ep': len(item['episode']), 
+    }
+  
+    charlist.append(char)
+  return charlist
+
+mainlist = []
+data = main_request(baseurl, endpoint, 2)
+#print(get_pages(data))
+for x in range(1, get_pages(data) + 1): #41 pages
+  print(x)
+  main_request(baseurl, endpoint, x)
+  mainlist.extend(parse_json(main_request(baseurl, endpoint, x)))
+
+#print(len(mainlist))
+
+df = pd.DataFrame(mainlist)
+
+#print(df.head(), df.tail())
+
+df.to_csv('charlist.csv', index=False)
+
